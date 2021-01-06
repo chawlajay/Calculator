b44a76991114f395c7f6b8ccfe646edd1cb16312
chawlajay <chawlajay111@gmail.com>
2021-01-07 00:10:11 +0530

    added addToDo function and eventlistener

--- To Do List/script.js
+++ To Do List/script.js
@@ -4,5 +4,30 @@ const dateElement=document.getElementById("date");
 
 const list=document.getElementById("list");
 
-const input=document.getElementById("item");
+const input=document.getElementById("input_item");
+
+function addToDo(toDo_item){
+const text= `<li class="item"><i class="far fa-circle" job="complete"> </i> <p class="text"> ${toDo_item} </p> <i class="far fa-trash-alt" job="delete"> </i> </li>`; 
+const position = "beforeend";
+
+list.insertAdjacentHTML(position,text);
+}
+
+
+document.addEventListener("keyup",function(event){
+
+if(event.keyCode == 13)
+{
+	const toDo = input.value;
+	
+	if(toDo)
+	{
+		addToDo(toDo);
+	}
+	input.value="";
+}
+
+});
+
+
 
