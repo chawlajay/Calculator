06c2106201e90da0e6fea167acbb0034908839f6
chawlajay <chawlajay111@gmail.com>
2021-03-10 21:08:00 +0530

    added function to get hex value and change color randomly

--- background-color/script.js
+++ background-color/script.js
@@ -9,3 +9,23 @@ function changeColor(){
     let randomNumber = Math.floor(Math.random()*colors.length);
     bodyBg.style.backgroundColor =colors[randomNumber];
 }
+
+/* hex color change code */
+const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hexColorBtn = document.querySelector('.hexColorBtn');
+const hexColor = document.querySelector('.hexColor');
+
+hexColorBtn.addEventListener('click',getHexColor);
+
+function getHexColor(){
+    let hexCol = '#';
+
+    for(let i=0;i<6;i++)
+    {
+        let randomNumber = Math.floor(Math.random()*hexNumbers.length);
+        hexCol+=hexNumbers[randomNumber];
+    }
+    bodyBg.style.backgroundColor =hexCol;
+    hexColor.innerHTML = hexCol;
+}
+
