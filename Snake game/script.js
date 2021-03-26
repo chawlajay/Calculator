6b65b4886d550b07f5e3803407e5609d2eb843f4
chawlajay <chawlajay111@gmail.com>
2021-03-26 20:57:31 +0530

    added logic and function for Game Over

--- Snake game/script.js
+++ Snake game/script.js
@@ -1,10 +1,22 @@
 // game loop will repeat itself again and again at some time interval
 
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED , getSnakeHead, snakeIntersection } from './snake.js';
 import { update as updateFood, draw as drawFood} from './food.js';
+import { outsideGrid } from './grid.js';
+
 var lastRenderTime=0;
+let gameOver = false;
+
 const gameBoard = document.querySelector('#game-board'); 
 function main(currentTime){
+
+    if(gameOver){
+        if(confirm('You Lost. Press OK to restart.')){
+            window.location='/';  // refresh page
+        }
+        return
+    }
+
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;  // into seconds
     
@@ -25,10 +37,15 @@ window.requestAnimationFrame(main);
 function update(){
 updateSnake();
 updateFood();
+checkDeath();
 }
 
 function draw(){
-    gameBoard.innerHTML='';
+gameBoard.innerHTML='';
 drawSnake(gameBoard);
 drawFood(gameBoard);
+}
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
