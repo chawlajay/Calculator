c15b0d8299b654452f7b5b1cb8dc90a5b431115c
chawlajay <chawlajay111@gmail.com>
2021-03-26 20:02:12 +0530

    added code to expand snake and capture food

--- Snake game/food.js
+++ Snake game/food.js
@@ -1,11 +1,16 @@
-let food = { x:0, y:0 };
+import { onSnake , expandSnake } from './snake.js';
 
+let food = { x:10, y:1 };
+const EXPANSION_RATE = 1;
 export function update(){
     // console.log('update snake');
-        
+    if(onSnake(food)){
+        expandSnake(EXPANSION_RATE);
+        food = {x:20, y:10};  // for now only
+    }
     }
     
-    export function draw(gameBoard){
+export function draw(gameBoard){
     // console.log('draw snake');
    
         const foodElement = document.createElement('div');
--- Snake game/snake.js
+++ Snake game/snake.js
@@ -1,9 +1,14 @@
 // all the code of snake goes in here
 import { getInputDirection } from "./input.js";
 export const SNAKE_SPEED = 5;
-const snakeBody = [ {x: 11, y: 11} ]
+const snakeBody = [ {x: 11, y: 11} ];
+
+let newSegments = 0;
+
+
 export function update(){
 // console.log('update snake');
+    addSegments();
     const inputDirection = getInputDirection()
     for(let i=snakeBody.length - 2; i>=0;i--)
     {
@@ -23,4 +28,26 @@ snakeBody.forEach(segment =>{
     snakeElement.classList.add('snake');
     gameBoard.appendChild(snakeElement);
 })
+}
+
+export function expandSnake(amount){
+    newSegments+=amount;
+}
+
+export function onSnake(position){
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position);
+    });
+}
+
+function equalPositions(pos1,pos2){
+    return (pos1.x===pos2.x && pos1.y===pos2.y);
+}
+
+function addSegments(){
+    for(let i=0;i<newSegments;i++)
+    {
+        snakeBody.push({...snakeBody[snakeBody.length-1]});
+    }
+    newSegments=0;
 }
