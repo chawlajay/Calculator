b1fdde8203911ef84f2b72bc9be07566d9fee089
chawlajay <chawlajay111@gmail.com>
2021-03-29 15:53:09 +0530

    added CSS and JS codes for 4 direction buttons

--- Snake game/input.js
+++ Snake game/input.js
@@ -1,6 +1,32 @@
 // file which takes all inputs
 let inputDirection = { x:0 , y:0 };
 let lastInputDirection = { x:0, y:0 };
+let arrowKeyUpButton = document.querySelector(".arrow_up");
+let arrowKeyDownButton = document.querySelector(".arrow_down");
+let arrowKeyLeftButton = document.querySelector(".arrow_left");
+let arrowKeyRightButton = document.querySelector(".arrow_right");
+
+arrowKeyUpButton.addEventListener('click', ()=>{
+    console.log('up');
+    if(lastInputDirection.y==0)
+    inputDirection = { x:0, y:-1};
+});
+
+arrowKeyDownButton.addEventListener('click', ()=>{
+    if(lastInputDirection.y==0)
+    inputDirection = { x:0, y:1};
+});
+
+arrowKeyLeftButton.addEventListener('click', ()=>{
+    if(lastInputDirection.x==0)
+    inputDirection = { x:-1, y:0};
+});
+
+arrowKeyRightButton.addEventListener('click', ()=>{
+    if(lastInputDirection.x==0)
+    inputDirection = { x:1, y:0};
+});
+
 window.addEventListener('keydown', e=>{
     switch(e.key){
         case 'ArrowUp':
@@ -16,13 +42,16 @@ window.addEventListener('keydown', e=>{
             inputDirection = { x:-1, y:0};
             break;
         case 'ArrowRight':
-                if(lastInputDirection.x!==0) break;
+            if(lastInputDirection.x!==0) break;
             inputDirection = { x:1, y:0};
             break;
     }
 });
 
+
+
 export function getInputDirection(){
+    
     lastInputDirection = inputDirection;
     return inputDirection;
 }
