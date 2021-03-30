b15a2fd89d12222d45190012c852c06225727a70
chawlajay <chawlajay111@gmail.com>
2021-03-30 12:52:33 +0530

    created script file initialised with canvas code

---
+++ SnakeGameCanvas/snake_script.js
@@ -0,0 +1,7 @@
+
+const drawCanvas = $('#drawCanvas')[0];
+// we get the canvas “2d context", which means that it will be drawn into a 2D space.
+const context = drawCanvas.getContext('2d');
+const width = $('#drawCanvas').width();
+const height = $('#drawCanvas').height();
+
