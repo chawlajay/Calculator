a90efc9a220b24f0287fec62cce4b68da2301758
chawlajay <chawlajay111@gmail.com>
2021-05-14 19:35:19 +0530

    added js code, to print the result of 1 game

--- rockPaperScissors/app.js
+++ rockPaperScissors/app.js
@@ -4,10 +4,13 @@ let scissors_icon=document.getElementById("s");
 
 let user_score = document.getElementById("user_score");
 let computer_score = document.getElementById("computer_score");
-let computer_choice;
+let computer_choice,user_choice;
 
 let choices=["Rock","Paper","Scissor"];  // rock - 0, paper - 1, scissor -2
+let result = document.querySelector(".result p");
+
 rock_icon.addEventListener('mousedown',()=>{
+    user_choice=0;
     console.log('rock mousedown clicked');
     rock_icon.style.transform ="scale(0.8)";
 
@@ -18,6 +21,8 @@ rock_icon.addEventListener('mousedown',()=>{
         // game draw
         rock_icon.style.animationName="draw_game";
         rock_icon.style.animationDuration="5s";
+
+        result.innerHTML = choices[user_choice] + "(user) is equal to " + choices[computer_choice] + "(comp). Game Draw!";
     }
     else if(computer_choice==1)   // rock - user & paper - comp
     {
@@ -26,6 +31,7 @@ rock_icon.addEventListener('mousedown',()=>{
         rock_icon.style.animationDuration="5s";
 
         computer_score.innerHTML = parseInt(computer_score.innerHTML) + 1;
+        result.innerHTML = choices[computer_choice] + "(comp) covers " + choices[user_choice] + "(user). You Lost.";
     }
     else
     {
@@ -34,6 +40,7 @@ rock_icon.addEventListener('mousedown',()=>{
         rock_icon.style.animationDuration="5s";
 
         user_score.innerHTML = parseInt(user_score.innerHTML) + 1;
+        result.innerHTML = choices[user_choice] + "(user) destroys " + choices[computer_choice] + "(comp). You Win.";
     }
 });
 
