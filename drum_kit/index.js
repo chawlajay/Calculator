dbbc76e5f2dbe176540550147b408237db091cc1
chawlajay <chawlajay111@gmail.com>
2021-06-19 00:07:38 +0530

    added event listener for key press to play drums

--- drum_kit/index.js
+++ drum_kit/index.js
@@ -1,39 +1,49 @@
+function make_sound(button_or_key_pressed){
+    switch(button_or_key_pressed)
+{
+    case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+        case "a":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+        break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+        case "d":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "k":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+}
+
+}
+
 for(let i=0;i<document.querySelectorAll(".drum").length;i++)
 {
 document.querySelectorAll(".drum")[i].addEventListener('click',function (){
 
     var buttonInnerHTML = this.innerHTML;
-    switch(buttonInnerHTML){
-        case "w":
-            var play_sound = new Audio("sounds/crash.mp3");
-            play_sound.play();
-        break;
-        case "a":
-            var play_sound = new Audio("sounds/kick-bass.mp3");
-            play_sound.play();
-        break;
-        case "s":
-            var play_sound = new Audio("sounds/snare.mp3");
-            play_sound.play();
-        break;
-        case "d":
-            var play_sound = new Audio("sounds/tom-3.mp3");
-            play_sound.play();
-        break;
-        case "j":
-            var play_sound = new Audio("sounds/tom-2.mp3");
-            play_sound.play();
-        break;
-        case "k":
-            var play_sound = new Audio("sounds/tom-1.mp3");
-            play_sound.play();
-        break;
-        case "l":
-            var play_sound = new Audio("sounds/tom-4.mp3");
-            play_sound.play();
-        break;
-        default: console.log("No button like that available!");
-        break;
-    }
+    make_sound(buttonInnerHTML);
 });
 }
+
+
+document.addEventListener("keypress", function(){
+var value_of_key_pressed = event.key;
+make_sound(value_of_key_pressed);
+});
