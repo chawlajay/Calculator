5ba4de6ebaf05296ccd9d30fa68893f42fef519f
chawlajay <chawlajay111@gmail.com>
2021-06-20 21:34:08 +0530

    removed text in btn, added function in game.js

--- simonGame/game.js
+++ simonGame/game.js
@@ -1,11 +1,35 @@
 let randomNumber=0;
 let buttonColors = ["red", "blue", "green", "yellow"];
 let randomChosenColor = buttonColors[randomNumber];
-let gamePattern = [];
-function newSequence(){
+let gamePattern = [], userClickedPattern = [];
+function nextSequence(){
     randomNumber = Math.floor(Math.random()*4);
+    randomChosenColor = buttonColors[randomNumber];
+    animatePress(randomChosenColor);
+    playSound(randomChosenColor);
 }
 
-newSequence();
-gamePattern.push(randomChosenColor);
+function playSound(name){
+    var soundFile = new Audio("sounds/"+name+".mp3");
+    soundFile.play();
+}
 
+function animatePress(name){
+    $("#"+name).fadeOut(50,function(){
+        $(this).addClass("pressed");
+    }).fadeIn(50,function(){
+        $(this).removeClass("pressed");
+    });
+}
+
+$(".btn").click(function(){
+    var userChosenColor = event.target.id;
+    userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);
+    playSound(userChosenColor);
+});
+
+$(document).keyPress(function(){
+    nextSequence();
+    gamePattern.push(randomChosenColor);
+});
