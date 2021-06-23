e78c12c0f61a336318800595e2618059ddf9000d
chawlajay <chawlajay111@gmail.com>
2021-06-23 12:02:42 +0530

    added starter files & some constants in js file

---
+++ 3D_cube_js/script.js
@@ -0,0 +1,10 @@
+const COLOR_BG = "black";
+const COLOR_CUBE = "yellow";
+const SPEED_X = "0.05";
+const SPEED_Y = "0.15";
+const SPEED_Z = "0.10";
+const POINT3D = function (x,y,z){
+this.x = x;
+this.y = y;
+this.z = z;
+}
