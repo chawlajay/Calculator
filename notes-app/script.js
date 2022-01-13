787ff1c5fbbc2419daaeb5139e1857deaa7a8a5e
chawlajay <chawlajay111@gmail.com>
2022-01-13 23:06:21 +0530

    add 'add note' button to add notes div

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,12 +1,42 @@
-const notesEl = document.querySelector(".notes");
-const editBtn = document.querySelector(".edit");
-const deleteBtn = document.querySelector(".delete");
+const addBtn = document.getElementById("add");
 
-const main = notesEl.querySelector(".main");
-const textArea = notesEl.querySelector("textarea");
+addBtn.addEventListener('click',()=>{
+    addNewNote();
+});
 
-editBtn.addEventListener('click',(e)=>{
-    console.log(e.target);
-    main.classList.toggle('hidden');
-    textArea.classList.toggle('hidden');
-})
+function addNewNote(){
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    note.innerHTML = `
+    <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <div class="main hidden"></div>
+    <textarea></textarea>
+    `;
+
+    const editBtn = note.querySelector(".edit");
+    const deleteBtn = note.querySelector(".delete");
+
+    const main = note.querySelector(".main");
+    const textArea = note.querySelector("textarea");
+    
+    editBtn.addEventListener('click',(e)=>{
+        // console.log(e.target);
+        main.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    });
+    
+    deleteBtn.addEventListener('click', ()=>{
+        note.remove();
+    });
+
+    textArea.addEventListener('input',(e)=>{
+        const {value} = e.target;
+        main.innerHTML = marked.parse(value);
+    });
+
+    document.body.appendChild(note);
+}
