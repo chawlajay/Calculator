32acc3eea03d1acaf857013062c3ace400bdf87f
chawlajay <chawlajay111@gmail.com>
2022-01-13 20:35:07 +0530

    add edit button code to toogle main & textarea

---
+++ notes-app/script.js
@@ -0,0 +1,12 @@
+const notesEl = document.querySelector(".notes");
+const editBtn = document.querySelector(".edit");
+const deleteBtn = document.querySelector(".delete");
+
+const main = notesEl.querySelector(".main");
+const textArea = notesEl.querySelector("textarea");
+
+editBtn.addEventListener('click',(e)=>{
+    console.log(e.target);
+    main.classList.toggle('hidden');
+    textArea.classList.toggle('hidden');
+})
