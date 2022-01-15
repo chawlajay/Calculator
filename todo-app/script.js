6dd85930e1f5a9c7c0f7011c8462b6b680d9ec3d
chawlajay <chawlajay111@gmail.com>
2022-01-15 23:44:03 +0530

    add updateLS to add & remove todos in localStorage

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,8 +1,12 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const todos = document.getElementById("todos");
+const todosUL = document.getElementById("todos");
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
+    addTodo(); 
+});
+
+function addTodo(){
     const todoText = input.value;
 
     if(todoText){
@@ -11,14 +15,30 @@ form.addEventListener('submit',(e)=>{
 
         todoEl.addEventListener('click',()=>{
             todoEl.classList.toggle('completed');
+            updateLS();
         });
 
         todoEl.addEventListener('contextmenu',(e) =>{
             e.preventDefault();
             todoEl.remove();
+            updateLS();
         });
-        const firstItem = todos.firstChild;
-        todos.insertBefore(todoEl,firstItem);
+        const firstItem = todosUL.firstChild;
+        todosUL.insertBefore(todoEl,firstItem);
         input.value = "";
-    } 
-});
+
+        updateLS();
+    }
+}
+
+function updateLS(){
+    const todosEl = document.querySelectorAll("li");
+    const todos = [];
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        });
+    });
+    localStorage.setItem("todos",JSON.stringify(todos));
+}
