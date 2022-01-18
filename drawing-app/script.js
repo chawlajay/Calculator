b5999f721579dfc559770eb4ef3db6b5d452847d
chawlajay <chawlajay111@gmail.com>
2022-01-18 23:36:33 +0530

    add random color button, styles color

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -3,9 +3,12 @@ const ctx = canvas.getContext('2d');
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const sizeEl = document.getElementById("size");
+const chooseColor = document.getElementById("color");
+const randomColorBtn = document.getElementById("random-color");
 
 let size = 10,x=100,y=40;
-let isPressed = false;
+let isPressed = false,randomColor = true,fixColor = "#000";
+let colorArray = ["#e34231","#1c7974"];
 setSizeCanvas(70);
 
 canvas.addEventListener('mousemove', (e)=>{
@@ -33,12 +36,34 @@ function drawCircle(x,y){
     ctx.beginPath();
     ctx.arc(x,y,size,0,2*Math.PI);
     let randomHexChar = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E'];
-    let randomColor = "#";
+    let randomColorValue = "#";
     for(let i=0;i<6;i++)
-    randomColor += randomHexChar[Math.floor(Math.random()*randomHexChar.length)];
-    ctx.fillStyle=randomColor;
+    randomColorValue += randomHexChar[Math.floor(Math.random()*randomHexChar.length)];
+
+    colorArray.push(randomColorValue);
+    colorArray = colorArray.slice(1);
+
+    if(randomColor){
+    let bgValue = "linear-gradient(120deg, ";
+    colorArray.forEach(color => {
+        bgValue += color;
+        bgValue += ",";
+    });
+     bgValue = bgValue.slice(0,-1);
+     bgValue += ')';
+    randomColorBtn.style.background = bgValue;
+    // console.log(bgValue);
+    ctx.fillStyle=randomColorValue;
+    }
+    else
+    ctx.fillStyle = fixColor;
+
     ctx.fill();
     ctx.stroke();
+}
+
+function updateSize(){
+    sizeEl.innerText = size;
 }
 
 increaseBtn.addEventListener('click', ()=>{
@@ -52,11 +77,17 @@ increaseBtn.addEventListener('click', ()=>{
 decreaseBtn.addEventListener('click', ()=>{
     size -= 5;
     if(size < 5)
-    size=5;
+    size=1;
 
     updateSize();
 });
 
-function updateSize(){
-    sizeEl.innerText = size;
-}
+chooseColor.addEventListener('change',()=>{
+console.log(chooseColor.value);
+fixColor = chooseColor.value;
+randomColor = false;
+});
+
+randomColorBtn.addEventListener('click', ()=>{
+randomColor=true;
+});
