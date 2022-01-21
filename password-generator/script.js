18fb7d38c6a2cd7a93f0c1b710e1a52ce2a6aca2
chawlajay <chawlajay111@gmail.com>
2022-01-21 11:33:18 +0530

    add elements to use through IDs

---
+++ password-generator/script.js
@@ -0,0 +1,13 @@
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lengthEl = document.getElementById("length");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl= document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+=";
