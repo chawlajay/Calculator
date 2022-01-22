285769a5129be2e62b37419f72daa97e70755a4f
chawlajay <chawlajay111@gmail.com>
2022-01-23 00:54:43 +0530

    add search by form event listener

--- weather-app/script.js
+++ weather-app/script.js
@@ -3,8 +3,8 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 getWeatherByLocation("surat");
-async function getWeatherByLocation(location){
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid="+owm_app_id+"&units=metric";
+async function getWeatherByLocation(city){
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+owm_app_id+"&units=metric";
     const resp = await fetch(url);
     const respData = await resp.json();
     console.log(respData);
@@ -12,15 +12,32 @@ async function getWeatherByLocation(location){
 }
 
 function addWeatherToPage(data){
-  const temp = data.main.temp;
-
   const weather = document.createElement("div");
   weather.classList.add("weather");
+
+  if(data.cod==200){
+  const temp = data.main.temp;
+  
   weather.innerHTML = `
   <small>It is</small>
   <h2>${temp}°C</h2>
   <p>in ${data.name}</p>
   `;
+  }
+  else{
+  
+  weather.innerHTML = `
+  <p>No city found - error!</p>
+  `;
+  }
 
+  main.innerHTML="";
   main.appendChild(weather);
 }
+
+form.addEventListener('submit',(e)=>{
+e.preventDefault();
+const city = search.value;
+if(city)
+getWeatherByLocation(city);
+});
