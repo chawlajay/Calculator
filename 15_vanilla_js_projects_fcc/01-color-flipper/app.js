ad405c1867697790a4ce5a56bd4573222e782a82
chawlajay <chawlajay111@gmail.com>
2022-02-08 14:28:45 +0530

    add js logic and code, all done

--- 15_vanilla_js_projects_fcc/01-color-flipper/app.js
+++ 15_vanilla_js_projects_fcc/01-color-flipper/app.js
@@ -1,1 +1,9 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["blue","green", "red", "rgba(133,122,200)", "#f15025","#f1f5f8","purple","yellow","orangered","brown","rebeccapurple"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click',function(){
+    let current_color = colors[Math.floor(Math.random()*colors.length)];
+    document.body.style.backgroundColor = current_color;
+    color.innerText = current_color;
+});
--- 15_vanilla_js_projects_fcc/01-color-flipper/hex.js
+++ 15_vanilla_js_projects_fcc/01-color-flipper/hex.js
@@ -1,1 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color_el = document.querySelector(".color");
+
+btn.addEventListener('click',function(){
+    let color = "#";
+    for(let i=0;i<6;i++)
+    color += hex[Math.floor(Math.random()*hex.length)];
+
+    document.body.style.backgroundColor = color;
+    color_el.innerText = color;
+});
+
