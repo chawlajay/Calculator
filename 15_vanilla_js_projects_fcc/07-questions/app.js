87b2fdd3b54887deebfa4b57e1dafc5032f463db
chawlajay <chawlajay111@gmail.com>
2022-02-09 11:56:52 +0530

    add js & css code, complete questions project

--- 15_vanilla_js_projects_fcc/07-questions/app.js
+++ 15_vanilla_js_projects_fcc/07-questions/app.js
@@ -1,2 +1,8 @@
-//using selectors inside the element
-// traversing the dom
+const questionEl = document.querySelectorAll(".question");
+
+for(let i=0;i<questionEl.length;i++){
+    let plusBtn = questionEl[i].querySelector(".question-btn");
+    plusBtn.addEventListener('click',()=>{
+    questionEl[i].classList.toggle("show-text");
+    });
+}
