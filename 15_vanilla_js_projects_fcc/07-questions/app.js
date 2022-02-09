7f4bd0c21d0b4667b5cc71f654ebd2b95578c794
chawlajay <chawlajay111@gmail.com>
2022-02-09 12:04:23 +0530

    add removeShowText() to remove class at each click

--- 15_vanilla_js_projects_fcc/07-questions/app.js
+++ 15_vanilla_js_projects_fcc/07-questions/app.js
@@ -3,6 +3,19 @@ const questionEl = document.querySelectorAll(".question");
 for(let i=0;i<questionEl.length;i++){
     let plusBtn = questionEl[i].querySelector(".question-btn");
     plusBtn.addEventListener('click',()=>{
-    questionEl[i].classList.toggle("show-text");
+        if(!questionEl[i].classList.contains("show-text"))
+        {
+            removeShowText();
+            questionEl[i].classList.add("show-text");
+        }
+        else
+        removeShowText();
     });
+}
+
+
+function removeShowText(){
+    for(let i=0;i<questionEl.length;i++){
+        questionEl[i].classList.remove("show-text");
+    }
 }
