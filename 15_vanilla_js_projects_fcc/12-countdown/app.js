39742d15968b63a251d40c2fe99a21ace898f9d0
chawlajay <chawlajay111@gmail.com>
2022-02-13 15:29:10 +0530

    set future date dynamically from JS

--- 15_vanilla_js_projects_fcc/12-countdown/app.js
+++ 15_vanilla_js_projects_fcc/12-countdown/app.js
@@ -21,3 +21,20 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveawayDate = document.getElementById("date");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2022,4,24,8,59,59,0);
+let options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric"
+};
+
+giveawayDate.innerHTML = futureDate.toLocaleDateString('en-US',options);
