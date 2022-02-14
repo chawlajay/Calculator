0a72bbb37ba2c9f3d60123f17105df5d48fc3fe2
chawlajay <chawlajay111@gmail.com>
2022-02-14 22:07:55 +0530

    add addItem() - show alert and displayAlert()

--- 15_vanilla_js_projects_fcc/14-grocery-list/app.js
+++ 15_vanilla_js_projects_fcc/14-grocery-list/app.js
@@ -11,9 +11,33 @@ let editElement;
 let editFlag = false;
 let editId='';
 // ****** EVENT LISTENERS **********
-
+form.addEventListener('submit',addItem);
 // ****** FUNCTIONS **********
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
 
+    if(value && !editFlag){
+        console.log("add item to list");
+    }
+    else if(value && editFlag){
+        console.log("editing");
+    }
+    else{
+        displayAlert("please enter value","danger")
+    }
+}
+
+function displayAlert(text,action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+    
+    setTimeout(function(){
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+    },1000);
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
