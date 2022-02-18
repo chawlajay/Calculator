e834b033e7e3213d9b44fdeb7f924216cc6507a8
chawlajay <chawlajay111@gmail.com>
2022-02-18 19:49:41 +0530

    add fontawesome heart ico instead of emoji

--- 10projects_1hour_florinpop/05-red_heart_rain/script.js
+++ 10projects_1hour_florinpop/05-red_heart_rain/script.js
@@ -1,9 +1,14 @@
 function createHeart(){
     const heart = document.createElement("div");
+    const heartIcon = document.createElement("i");
+
+    heartIcon.classList.add("fas","fa-heart");
+    heart.appendChild(heartIcon);
+
     heart.classList.add("heart");
     heart.style.left = Math.random() * 100 + "vw";
-    heart.innerText="❤️";
     heart.style.animationDuration = Math.random()*2 + 3 + "s";
+    
     document.body.appendChild(heart);
 
     setTimeout(()=>{
