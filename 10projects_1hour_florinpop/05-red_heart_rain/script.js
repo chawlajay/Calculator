6a16961b1c84d3532d7480b8005e61300208101b
chawlajay <chawlajay111@gmail.com>
2022-02-18 22:30:37 +0530

    add code in html and script to import json data

--- 10projects_1hour_florinpop/05-red_heart_rain/script.js
+++ 10projects_1hour_florinpop/05-red_heart_rain/script.js
@@ -1,3 +1,5 @@
+import allCssColors from "./cssColors.json" assert { type: "json" };
+
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const rightArrowBtn = document.getElementById("rightArrowBtn");
 const colorBtn = document.getElementById("colorBtn");
